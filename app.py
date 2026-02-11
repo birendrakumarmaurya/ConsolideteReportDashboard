44ab4ac1cc83046811ca478c1f71fb1e171f83f3
Birendra <birendrakumarmaurya@gmail.com>
2026-02-11 09:41:58 +0530

    Ratio change

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ def parse_docx(file_stream, filename):
             avg_student = talk_ratio['student']
 
     # Rating ----------------------------------------------------------- 3.5
-    score = 4 + (len(glows) * 0.25) - (len(grows) * 0.15)
+    score = 3.7 + (len(glows) * 0.25) - (len(grows) * 0.15)
     if avg_student >= 55: score += 0.2
     if avg_student <= 30: score -= 0.2
     rating = round(max(2.5, min(5.0, score)), 1)
@@ -176,4 +176,5 @@ if uploaded_files:
 
 elif not uploaded_files:
     st.info("Please upload .docx files to start.")
+
 
